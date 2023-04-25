4f5fd1c09559a4a5e6f6108fdea572216048e75d
akhil <akhil@gmail.com>
2023-04-25 16:43:44 +0530

    terraform update

--- sample.tf/ec2.tf
+++ sample.tf/ec2.tf
@@ -8,7 +8,7 @@ output "aws-ami" {
 }
 
 resource "aws_instance" "frontend" {
-  ami           = "ami-0bb6af715826253bf"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
