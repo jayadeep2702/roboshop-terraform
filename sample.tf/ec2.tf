560a6fc716f762c48e1992c9d5a93e289583d90f
akhil <akhil@gmail.com>
2023-04-28 16:41:51 +0530

    terraform update

--- sample.tf/ec2.tf
+++ sample.tf/ec2.tf
@@ -1,7 +1,10 @@
-data "aws_ami" "frontend" {
+data "aws_ami" "centos" {
   owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
+}
+output "aws_ami" {
+  value = data.aws_ami.centos.image_id
 }
 
 resource "aws_instance" "frontend" {
