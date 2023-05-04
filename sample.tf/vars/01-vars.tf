58ca486a07a80405c0c68dcb6bd2f482a48bebb8
akhil <akhil@gmail.com>
2023-05-04 13:26:02 +0530

    terraform update

--- sample.tf/vars/01-vars.tf
+++ sample.tf/vars/01-vars.tf
@@ -9,3 +9,23 @@ output "sample_string" {
 output "sample_string1" {
   value = "value of the string = ${var.sample_string}"
 }
+
+variable "sample_number" {
+  default = 2702
+}
+
+variable "sample_boolean" {
+  default = true
+}
+
+output "sample_number" {
+  value = var.sample_number
+}
+
+output "sample_boolean" {
+  value = var.sample_boolean
+}
+
+
+
+
