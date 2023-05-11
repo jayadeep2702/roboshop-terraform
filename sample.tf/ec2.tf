3b58428b061be1e1a40368ac62145daf747b444a
akhil <akhil@gmail.com>
2023-05-11 20:22:50 +0530

    terraform update

--- sample.tf/ec2.tf
+++ sample.tf/ec2.tf
@@ -1,20 +1,12 @@
-data "aws_ami" "frontend" {
+data "aws_ami" "centos" {
   owners      = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-}
-output "ami_image" {
-  value = data.aws_ami.frontend.image_id
 }
 
-data "aws_ami" "mongobd" {
-  owners      = ["973714476881"]
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-}
 
   resource "aws_instance" "frontend" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -22,12 +14,9 @@ data "aws_ami" "mongobd" {
   }
 }
 
-output "frontend" {
-  value = aws_instance.frontend.public_ip
-}
 
 resource "aws_instance" "mongodb" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -36,7 +25,7 @@ resource "aws_instance" "mongodb" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -44,7 +33,7 @@ resource "aws_instance" "catalogue" {
   }
 }
 resource "aws_instance" "redis" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -52,7 +41,7 @@ resource "aws_instance" "redis" {
   }
 }
 resource "aws_instance" "cart" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -60,7 +49,7 @@ resource "aws_instance" "cart" {
   }
 }
 resource "aws_instance" "user" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -68,7 +57,7 @@ resource "aws_instance" "user" {
   }
 }
 resource "aws_instance" "mysql" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -76,7 +65,7 @@ resource "aws_instance" "mysql" {
   }
 }
 resource "aws_instance" "shipping" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -84,7 +73,7 @@ resource "aws_instance" "shipping" {
   }
 }
 resource "aws_instance" "rabbitmq" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
@@ -92,7 +81,7 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 resource "aws_instance" "payment" {
-  ami           = "ami-0b5a2b5b8f2be4ec2"
+  ami           =  data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
