858c15d075a06254f79f73dc04dd1a38b6427a29
akhil <akhil@gmail.com>
2023-05-16 15:26:18 +0530

    terraform update

--- sample.tf/servers.tf/main.tf
+++ sample.tf/servers.tf/main.tf
@@ -5,6 +5,7 @@ variable "components" {
 resource "aws_instance" "instance" {
   count = length(var.components)
   ami   = "ami-0b5a2b5b8f2be4ec2"
+  instance_type = "t3.micro"
 
   tags = {
     Name = var.components[count.index]
