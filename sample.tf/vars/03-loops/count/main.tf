e65c711ce326583330defe941a35c6b2b2b590d5
akhil <akhil@gmail.com>
2023-05-16 14:34:03 +0530

    terraform update

--- sample.tf/vars/03-loops/count/main.tf
+++ sample.tf/vars/03-loops/count/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "null" {
   count = 6
 }
+
+resource "null_resource" "bikes" {
+  count = length(var.list)
+}
+
+variable "list" {
+  default = ["yamaha","honda","royal"]
+}
