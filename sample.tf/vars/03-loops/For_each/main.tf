8aa31900be404207383be4c6e0e8498b4b17fa78
akhil <akhil@gmail.com>
2023-05-17 00:37:09 +0530

    terraform update

--- sample.tf/vars/03-loops/For_each/main.tf
+++ sample.tf/vars/03-loops/For_each/main.tf
@@ -13,3 +13,29 @@ variable "fruits" {
     ornage = 200
   }
 }
+
+resource "null_resource" "fruits1" {
+  for_each = var.fruits1
+
+  provisioner "local-exec" {
+    command = "echo fruit name - ${each.key} -${each.value["count"]}"
+  }
+  }
+
+variable "fruits1" {
+  default = {
+    apple = {
+  name  = apple
+  count = 200
+ }
+    banana = {
+  name  = banana
+  count = 500
+}
+   orange = {
+  name  = orange
+  count = 400
+   }
+ }
+}
+
